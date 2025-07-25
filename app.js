f03a01296bdfb75fda574cd592aef03b35f1e664
genesis06 <gss572@ftr.com>
2025-07-24 21:04:10 -0600

    App inicial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const stockController = require('./controllers/stock');
+const app = express();
+
+// Ruta GET sencilla
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Ruta GET sencilla
+app.get('/api/inventario', (req, res) => {
+
+  const data = stockController.getStock();
+  res.json(data);
+});
+
+// Puerto en el que el servidor escuchará
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
---
+++ controllers/stock.js
@@ -0,0 +1,26 @@
+const xlsx = require('xlsx');
+const path = require('path');
+
+module.exports = {
+  getStock: () => {
+    // Ruta al archivo Excel
+    const filePath = path.join(__dirname, '../data/stock.xlsx');
+
+    // Leer el archivo
+    const workbook = xlsx.readFile(filePath);
+
+    // Obtener el nombre de la primera hoja
+    const sheetName = workbook.SheetNames[0];
+
+    // Obtener los datos de la hoja
+    const worksheet = workbook.Sheets[sheetName];
+
+    // Convertir a JSON
+    const data = xlsx.utils.sheet_to_json(worksheet);
+
+    // Mostrar en consola
+    console.log('Datos del Excel:', data);
+
+    return data
+  }
+};
